ff3b603300dd15a025243c23bb012c560a41bf50
Test <test@test.te>
2022-09-13 11:04:47 +0300

    Добавление действия
    # Conflicts:
    #	DirRX.HelpDesk/DirRX.HelpDesk.ClientBase/Document/DocumentActions.cs
    #	DirRX.HelpDesk/DirRX.HelpDesk.Shared/Document/Document.mtd
    #	DirRX.HelpDesk/DirRX.HelpDesk.Shared/Document/DocumentSystem.resx
    #	DirRX.HelpDesk/DirRX.HelpDesk.Shared/Module.mtd

---
+++ DirRX.HelpDesk/DirRX.HelpDesk.ClientBase/Document/DocumentActions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using DirRX.HelpDesk.Document;
+
+namespace DirRX.HelpDesk.Client
+{
+  partial class DocumentActions
+  {
+    public virtual void Action(Sungero.Domain.Client.ExecuteActionArgs e)
+    {
+      
+    }
+
+    public virtual bool CanAction(Sungero.Domain.Client.CanExecuteActionArgs e)
+    {
+      return true;
+    }
+
+  }
+
+}
