fdfb10ccf8e410831272ef270f0428d4e5074615
Test <test@test.te>
2022-09-13 13:14:43 +0300

    Added DeletedDoc
    # Conflicts:
    #	starkov.Module1/starkov.Module1.Shared/Module.mtd

---
+++ starkov.Module1/starkov.Module1.Shared/DeletedDoc/DeletedDocHandlers.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using starkov.Module1.DeletedDoc;
+
+namespace starkov.Module1
+{
+  partial class DeletedDocSharedHandlers
+  {
+
+  }
+}
